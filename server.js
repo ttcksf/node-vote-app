39cec35cce16e4b67fb9350fa18c71a7b27f85dc
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-29 01:23:58 +0900

    first

---
+++ server.js
@@ -0,0 +1,24 @@
+const { Socket } = require('socket.io');
+
+const io = require('socket.io')(8000, { cors: { origin: '*' } });
+
+let total = 0;
+let votingPolls = {
+  'html-css': 0,
+  javascript: 0,
+  react: 0,
+  'react-typescript': 0,
+  nodejs: 0,
+  nextjs: 0,
+};
+
+io.on('connection', (socket) => {
+  socket.emit('update', { votingPolls, total });
+  socket.on('send-vote', (lang) => {
+    total += 1;
+    console.log(lang);
+    votingPolls[lang] += 1;
+    socket.broadcast.emit('receive-vote', { votingPolls, total });
+    socket.emit('update', { votingPolls, total });
+  });
+});
